a63a46661a067ca89b8d073c05c833ebf04e7056
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-07 21:27:41 -0300

    first

---
+++ app/src/main/java/com/vinighub/listalazy/model/Game.kt
@@ -0,0 +1,10 @@
+package com.vinighub.listalazy.model
+
+data class Game(
+    val id: Long = 0,
+    val title: String = "",
+    val studio: String = "",
+    val releaseYear: Int = 0
+    )
+{
+}
